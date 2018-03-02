fb9234788e3c827f3ab445eb29332ca1e904e871
john <36755676+joooooooooooooooooooo@users.noreply.github.com>
2018-03-02 14:11:49 +0530

    Create hai5.py

---
+++ hai5.py
@@ -0,0 +1,10 @@
+num1=10
+num2=22
+num3=14
+if(num1>=num2) and (num1>=num3):
+    largest=num1
+elif(num2>=num1) and (num2>=num3):
+    largest=num2
+else:
+    largest=num3
+print("the largest number is",num1,",num2","and","num3","is",largest)
